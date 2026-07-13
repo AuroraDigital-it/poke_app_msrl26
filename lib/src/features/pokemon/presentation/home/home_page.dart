9fbf767a0bcc988a16e848c26ef22627cc422a52
Elia Tolin <eliatolin@gmail.com>
2026-07-13 16:42:37 +0200

    dev: biometrics

--- lib/src/features/pokemon/presentation/home/home_page.dart
+++ lib/src/features/pokemon/presentation/home/home_page.dart
@@ -1,8 +1,11 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:poke_app/src/client/talker/talker_client.dart';
 import 'package:poke_app/src/common/widget/async_view/async_view.dart';
 import 'package:poke_app/src/features/pokemon/presentation/home/controller/home_controller.dart';
@@ -32,6 +35,26 @@ class HomePage extends HookConsumerWidget {
       appBar: AppBar(
         title: Text("Home Page"),
         actions: [
+          IconButton(
+            icon: Icon(Icons.fingerprint),
+            onPressed: () async {
+              final LocalAuthentication auth = LocalAuthentication();
+              final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+              final bool canAuthenticate =
+                  canAuthenticateWithBiometrics || await auth.isDeviceSupported();
+              log('canAuthenticateWithBiometrics: $canAuthenticateWithBiometrics');
+              log('canAuthenticate: $canAuthenticate');
+              final availableBiometrics = await auth.getAvailableBiometrics();
+              log('availableBiometrics: $availableBiometrics');
+              try {
+                final bool didAuthenticate = await auth.authenticate(
+                  localizedReason: 'Please authenticate to show account balance',
+                  biometricOnly: false,
+                );
+                log('didAuthenticate: $didAuthenticate');
+              } on Exception catch (_) {}
+            },
+          ),
           IconButton(
             icon: Icon(Icons.insert_emoticon),
             onPressed: () {
@@ -94,9 +117,7 @@ class HomePage extends HookConsumerWidget {
                           ),
                           trailing: IconButton(
                             onPressed: () => controller.setFavoritePokemon(pokemon.id),
-                            icon: Icon(
-                              isFavorite ? Icons.favorite : Icons.favorite_border,
-                            ),
+                            icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
                           ),
                           title: Text(pokemon.name.toUpperCase()),
                           subtitle: Text('ID: ${pokemon.id}'),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_darwin
+import local_auth_darwin
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
@@ -13,6 +14,7 @@ import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
